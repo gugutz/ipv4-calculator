bc421e6f44be24951f3f8ee5bcbe938260dfc6e0
Gustavo <gugutz@gmail.com>
2018-01-24 19:00:04 -0200

    Rename src/calcular-ips-rede/calcular_ips_rede.c to main.c

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>  // colocada para utilizar a funÁ„o de potencializaÁ„o "pow (int n, int n)"
-#include <locale.h> // necess·rio para aceitar acentos no cÛdigo
+#include <math.h>  // colocada para utilizar a fun√ß√£o de potencializa√ß√£o "pow (int n, int n)"
+#include <locale.h> // necess√°rio para aceitar acentos no c√≥digo
 
 /*
-	IFSC - GEST√O EM TI - L”GICA DE PROGRAMA«√O
-	TRABALHO EM C - CALCULAR ENDERE«OS DA REDE
+	IFSC - GEST√ÉO EM TI - L√ìGICA DE PROGRAMA√á√ÉO
+	TRABALHO EM C - CALCULAR ENDERE√áOS DA REDE
 
 	ALUNO:	GUSTAVO PEREIRA BORGES	
 */
@@ -15,69 +15,69 @@ int main(int argc, char *argv[]) {
 	
 	// mtheuew
 	// ******************************************************************
-	// *************** DECLARA«√O DE VAR¡IVEIS DO PROGRAMA **************
+	// *************** DECLARA√á√ÉO DE VAR√ÅIVEIS DO PROGRAMA **************
 	// ****************************************************************** 
 	
 
-	int ip[4], ipbin[4][8];			// VETOR DO IP INSERIDO PELO USU¡RIO
-	int mask[4], maskbin[4][8];			// VETOR DA M¡SCARA DE SUBREDE
-	int net[4], netbin[4][8];			// VETOR DO ENDERE«O DE REDE	
-	int bcast[4], bcastbin[4][8];   // VETOR DO ENDERE«O DE BROADCAST
+	int ip[4], ipbin[4][8];			// VETOR DO IP INSERIDO PELO USU√ÅRIO
+	int mask[4], maskbin[4][8];			// VETOR DA M√ÅSCARA DE SUBREDE
+	int net[4], netbin[4][8];			// VETOR DO ENDERE√áO DE REDE	
+	int bcast[4], bcastbin[4][8];   // VETOR DO ENDERE√áO DE BROADCAST
 	int wildcard[4];
-//	int i_bcast = 0; 	// variavel para controle da operaÁ„o que acha o endereÁo de broadcast
+//	int i_bcast = 0; 	// variavel para controle da opera√ß√£o que acha o endere√ßo de broadcast
 	
-	int bitsrede, bitshosts, bits;  // vari·veis para armazenar a quantidade de bits para a rede, quantidade de bits para hosts
+	int bitsrede, bitshosts, bits;  // vari√°veis para armazenar a quantidade de bits para a rede, quantidade de bits para hosts
 	int qtdeips, qtdehosts;
 	
-	int i, u, n;	 //VARIAVEIS PARA CONTROLE DE OPERA«’ES WHILE E FOR DENTRO ALGORITIMO;
-	int resto; 		// vari·vel para controle de operaÁıes de conversıes de base numÈrica.
-	char p;	// VARI¡VEL PARA PODER TRABALHAR COM PONTOS ENTRES OS NUMEROS DOS ENDERE«OS
+	int i, u, n;	 //VARIAVEIS PARA CONTROLE DE OPERA√á√ïES WHILE E FOR DENTRO ALGORITIMO;
+	int resto; 		// vari√°vel para controle de opera√ß√µes de convers√µes de base num√©rica.
+	char p;	// VARI√ÅVEL PARA PODER TRABALHAR COM PONTOS ENTRES OS NUMEROS DOS ENDERE√áOS
 	char loop;		
 		
-	setlocale(LC_ALL, "Portuguese");  // modifica configuraÁıes de linguagem para PortuguÈs, permitindo acentos e outras modificaÁıes.
+	setlocale(LC_ALL, "Portuguese");  // modifica configura√ß√µes de linguagem para Portugu√©s, permitindo acentos e outras modifica√ß√µes.
 
 
 
 	// ******************************************************************
-	// ***************** ENTRADA DE DADOS PELO USU¡RIO ******************
+	// ***************** ENTRADA DE DADOS PELO USU√ÅRIO ******************
 	// ****************************************************************** 
 	
-	inputdados:  // cria um label para o qual eu posso retornar no meio ou no final do cÛdigo usando o comando "goto", criando um loop
+	inputdados:  // cria um label para o qual eu posso retornar no meio ou no final do c√≥digo usando o comando "goto", criando um loop
 
 
-	// PEDE O ENDERE«O IP
+	// PEDE O ENDERE√áO IP
 	printf("\nDigite o endereco IP: ");
-	// A SOLU«√O AQUI USADA … DIVIDIR O IP EM 4 OCTETOS PARA FACILITAR AS OPERA«’ES &AND E OUTRAS
+	// A SOLU√á√ÉO AQUI USADA √â DIVIDIR O IP EM 4 OCTETOS PARA FACILITAR AS OPERA√á√ïES &AND E OUTRAS
 	scanf("%d %c %d %c %d %c %d", &ip[0], &p, &ip[1], &p, &ip[2], &p, &ip[3]);
 	// *******************************************************
 	
-	// PEDE A M¡SCARA PARA O USU¡RIO, DIVIDINDO-A EM 8 OCTETOS
+	// PEDE A M√ÅSCARA PARA O USU√ÅRIO, DIVIDINDO-A EM 8 OCTETOS
 	digitarmascara:
-	printf("\nDigite a m·scara de sub-rede: ");
+	printf("\nDigite a m√°scara de sub-rede: ");
 	scanf("%d %c %d %c %d %c %d", &mask[0], &p, &mask[1], &p, &mask[2], &p, &mask[3]);
 	// *******************************************************
 		
-	// verifica se os valores digitados para a subm·scara respeitam os padrıes
+	// verifica se os valores digitados para a subm√°scara respeitam os padr√µes
 	for (i=3; i>=0; --i) {
 		if (mask[i]==0  || mask[i]==128 || mask[i]==192 || mask[i]==224 || mask[i]==240 || mask[i]==248 || mask[i]==252 || mask[i]==254 || mask[i]==255) {
 			i++;
 			i--;
 		}  // endif
 		else {
-			printf("\nVocÍ digitou um valor invalido para m·scara de rede.\nOs valores permitidos s„o: 0 | 128 | 192 | 224 | 240 | 248 | 252 | 254 | 255\n");
-			i = 3;  // reseta a vari·vel de controle
+			printf("\nVoc√™ digitou um valor invalido para m√°scara de rede.\nOs valores permitidos s√£o: 0 | 128 | 192 | 224 | 240 | 248 | 252 | 254 | 255\n");
+			i = 3;  // reseta a vari√°vel de controle
 			goto digitarmascara;
 		} // endelse
 	}  // endfor
 	// *******************************************************
 	
-	// MONTA O IP DA REDE, TAMB…M DIVIDINDO EM OCTETOS
+	// MONTA O IP DA REDE, TAMB√âM DIVIDINDO EM OCTETOS
 	for (i=3 ; i>=0 ; i--) {
 		net[i] = ip[i] & mask[i];	
 	}
 	// *******************************************************
 
-	// monta o endereÁo wildcard da rede
+	// monta o endere√ßo wildcard da rede
 	for (i=3 ; i>=0 ; i--) {
 		wildcard[i] = 255 - mask[i];	
 	}
@@ -86,12 +86,12 @@ int main(int argc, char *argv[]) {
 	// ********* MOSTRANDO O IP DIGITADO E A MASCARA EM BINARIO *********
 	// ******************************************************************
 	
-	// para esta convers„o, usarei uma matriz com 4 vari·veis de 8 indices cada
+	// para esta convers√£o, usarei uma matriz com 4 vari√°veis de 8 indices cada
 	// ipbin[4][8]
 	
 	// CONVERTER PRA BINARIO
 	
-	printf("\n\nO ip digitado em forma bin·ria È: \n");
+	printf("\n\nO ip digitado em forma bin√°ria √©: \n");
 	for(i = 0; i <= 3; i++) {
 		n = ip[i];
 		for(u = 7; u >= 0; u--) {
@@ -107,7 +107,7 @@ int main(int argc, char *argv[]) {
 	printf("\n\n");
 	
 	
-	printf("A m·scara digitada em forma bin·ria È: \n");
+	printf("A m√°scara digitada em forma bin√°ria √©: \n");
 
 	for(i = 0; i <= 3; i++) {
 		n = mask[i];
@@ -123,7 +123,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	printf("\n\n");
-	//printf("IP digitado em forma bin·ria: %b %c %b %c %b %c %b\n\n", ipbin[0], p, ipbin[1], p, ipbin[2], p, ipbin[3]);	
+	//printf("IP digitado em forma bin√°ria: %b %c %b %c %b %c %b\n\n", ipbin[0], p, ipbin[1], p, ipbin[2], p, ipbin[3]);	
 	
 	
 	
@@ -131,11 +131,11 @@ int main(int argc, char *argv[]) {
 	// ************* ENCONTRANDO A QTDE DE BITS PARA REDE ***************
 	// ******************************************************************
 
-	// AS OPERA«’ES ABAIXO DEDUZEM QUANTOS BITS TEM A REDE, BASEADO NA QUANTIDADE DE OCTETOS COMPLETAMENTE PREENCHIDOS
+	// AS OPERA√á√ïES ABAIXO DEDUZEM QUANTOS BITS TEM A REDE, BASEADO NA QUANTIDADE DE OCTETOS COMPLETAMENTE PREENCHIDOS
 	bitsrede = 0;
 	for (i = 3; i>=0 ; i--) {
 		u = mask[i];
-		if (u > 1) { // esta operaÁ„o sÛ ser· feita se o valor do octeto n„o for 0, pois se for a rede n„o est· usando ele.
+		if (u > 1) { // esta opera√ß√£o s√≥ ser√° feita se o valor do octeto n√£o for 0, pois se for a rede n√£o est√° usando ele.
 			bits = 128;
 				while ( u > bits ) {
 					bitsrede += 1;
@@ -156,7 +156,7 @@ int main(int argc, char *argv[]) {
 	
 	// ******************************************************************
 	// *********** ENCONTRANDO A QTDE DE IPS DECIMAIS EM ****************
-	// *********** TODOS OS OCTETOS N√O RESERVADOS PELA REDE ************
+	// *********** TODOS OS OCTETOS N√ÉO RESERVADOS PELA REDE ************
 	// ******************************************************************
 	
 	n = bitshosts; // para poder operar o valor de bits para hosts sem perder ele	
@@ -165,15 +165,15 @@ int main(int argc, char *argv[]) {
 		n -= 8;
 	}  //endwhile
 	
-	// SE A QUANTIDADE DE BITS PARA HOSTS FOR 8 OU MENOS, ENT√O 
-	// A QUANTIDADE DE IPS DISPONÕVEIS … IGUAL A 2 ELEVADO A N
+	// SE A QUANTIDADE DE BITS PARA HOSTS FOR 8 OU MENOS, ENT√ÉO 
+	// A QUANTIDADE DE IPS DISPON√çVEIS √â IGUAL A 2 ELEVADO A N
 	if (n<=8 && n > 0) {
 		qtdeips += (pow(2,n)) - 1;
 	} //endif
 
 
 	// ******************************************************************
-	// ************** 	ENCONTRANDO O ENDERE«O DE BROADCAST	 ************
+	// ************** 	ENCONTRANDO O ENDERE√áO DE BROADCAST	 ************
 	// ******************************************************************
 
 	for (i=3 ; i>= 0 ; i--) {
@@ -191,7 +191,7 @@ int main(int argc, char *argv[]) {
 		
 	
 	// ******************************************************************
-	// ****** MOSTRAR OS DADOS DA REDE PARA O USU¡RIO DO PROGRAMA *******
+	// ****** MOSTRAR OS DADOS DA REDE PARA O USU√ÅRIO DO PROGRAMA *******
 	// ****************************************************************** 
 	
 	// QTDE DE BITS PARA REDE
@@ -202,25 +202,25 @@ int main(int argc, char *argv[]) {
 	
 	printf("Soma da quantidade de IPs disponiveis em cada octeto: %d \n\n", qtdeips);  // usando a variavel qtdehosts, pq a qtdeips ja foi modificada com operacoes no final do codigo
 	
-	// QTDE TOTAL DE HOSTS DISPONÕVEIS
+	// QTDE TOTAL DE HOSTS DISPON√çVEIS
 	printf("Quantidade de hosts (2n - 2): %d \n\n", qtdehosts);
 		
-	// ENDERE«O DA REDE
+	// ENDERE√áO DA REDE
 	printf("Endereco da rede: %d %c %d %c %d %c %d\n\n", net[0], p, net[1], p, net[2], p, net[3]);
 	
-	// ENDERE«O WILDCARD DA REDE
+	// ENDERE√áO WILDCARD DA REDE
 	printf("Endereco de Wildcard: %d %c %d %c %d %c %d\n\n", wildcard[0], p, wildcard[1], p, wildcard[2], p, wildcard[3]);
 	
 	// PRIMEIRO IP DA FAIXA
 	printf("O primeiro IP disponivel na faixa eh: %d%c%d%c%d%c%d\n\n", net[0], p, net[1],  p, net[2], p, (net[3] + 1) );
 	
-	// ⁄LTIMO IP DA FAIXA
-	printf("O ˙ltimo IP disponivel na faixa eh: %d%c%d%c%d%c%d\n\n", bcast[0], p, bcast[1],  p, bcast[2], p, bcast[3] -1 );
+	// √öLTIMO IP DA FAIXA
+	printf("O √∫ltimo IP disponivel na faixa eh: %d%c%d%c%d%c%d\n\n", bcast[0], p, bcast[1],  p, bcast[2], p, bcast[3] -1 );
 	
 	// IP DE BROADCAST DA REDE
-	printf("O endereÁo de broadcast da rede eh: %d%c%d%c%d%c%d \n\n", bcast[0], p, bcast[1],  p, bcast[2], p, bcast[3] );
+	printf("O endere√ßo de broadcast da rede eh: %d%c%d%c%d%c%d \n\n", bcast[0], p, bcast[1],  p, bcast[2], p, bcast[3] );
 	
-	// IP DE GATEWAY NO PADR√O CISCO (⁄LTIMO IP DISNPONÕVEL)
+	// IP DE GATEWAY NO PADR√ÉO CISCO (√öLTIMO IP DISNPON√çVEL)
 	printf("O gateway da rede eh: %d%c%d%c%d%c%d \n\n", bcast[0], p, bcast[1],  p, bcast[2], p, bcast[3] - 1 );
 
 
@@ -228,13 +228,13 @@ int main(int argc, char *argv[]) {
 	// **************** FIM DO PROGRAMA -> LOOP *************************
 	// ****************************************************************** 
 	
-	// PERGUNTA SE O USU¡RIO QUER DIGITAR UM NOVO ENDERE«O.
-	// SE A RESPOSTA FOR SIM, VOLTA PARA O COME«O DO C”DIGO
-	// CASO N√O QUEIRA, TERMINA O C”DIGO
-	printf("Deseja digitar um novo endereÁo? (S / N)");
+	// PERGUNTA SE O USU√ÅRIO QUER DIGITAR UM NOVO ENDERE√áO.
+	// SE A RESPOSTA FOR SIM, VOLTA PARA O COME√áO DO C√ìDIGO
+	// CASO N√ÉO QUEIRA, TERMINA O C√ìDIGO
+	printf("Deseja digitar um novo endere√ßo? (S / N)");
 	scanf("%s" , &loop);
 	if (loop == 's' || loop == 'S' ) {
-		goto inputdados; // recomeÁa a execuÁ„o a partir da label inputdados, no comeÁo do codigo
+		goto inputdados; // recome√ßa a execu√ß√£o a partir da label inputdados, no come√ßo do codigo
 	}
 	else{
 		system("pause");	
